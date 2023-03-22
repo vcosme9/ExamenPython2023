6854be0f86f2bf1a3b85471c7aec72902a0265ae
vcosme9 <36443424+vcosme9@users.noreply.github.com>
2023-03-22 10:30:32 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -2,16 +2,28 @@ import csv
 
 class Functions:
 
-    #def __init__(self):
-        #self.__file = file
+    def __init__(self):
+        self.__dicctionary = {}
 
     def read_data(self):
+        cont = 1
+        
+        etiquetas = ["type", "fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", ]
+
         with open('winequality.csv', 'r') as file:
             reader = csv.reader(file)
-            for row in reader:
-                print(row)
 
-            [None] + [{} for row in reader]
+            for row in reader:
+                #print(row)
+                dato = f"dato{str(cont)}"
+                self.__dicctionary = {dato}
+                cont += 1
+                for item in row:
+                    self.__dicctionary[dato]
+
+                
+
+            
 
 
 
